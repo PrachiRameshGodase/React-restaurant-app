209843af842a13eb85e8f1e43b15200b3e6fee63
PrachiRameshGodase <prachigodase2@gmail.com>
2023-05-30 17:12:51 +0530

    Adding a Form inside available meal list

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import MealItemForm from './MealItemForm';
 import classes from "./MealItem.module.css"
 
 function MealItem(props) {
@@ -11,7 +12,7 @@ function MealItem(props) {
         <div className={classes.price}>{price}</div>
         </div>
       <div>
-        
+        <MealItemForm/>
       </div>
     </li>
   )
