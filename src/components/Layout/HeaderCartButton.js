8680d102da4015cd620a10cd084bcec6c6176377
PrachiRameshGodase <prachigodase2@gmail.com>
2023-05-31 18:56:27 +0530

    Showing and Enabling the delete of Cart Items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,39 @@
-import React from "react";
-import { useContext } from "react";
+import { useContext,useEffect,useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/Cartlcon";
 import classes from "./HeaderCartButton.module.css";
 
 export default function HeaderCartButton(props) {
+  const [btnIsHighLighted,setbtnIsHighLighted]=useState(false);
   const cartCtx=useContext(CartContext);
+  const {items}=cartCtx;
+
   const numberOfCartItems=cartCtx.items.reduce((currNumber,item)=>{
     return currNumber+item.amount;
   },0);
+
+  
+
+  const btnClasses=`${classes.button} ${btnIsHighLighted ? classes.bump:""}`
+
+  useEffect(()=>{
+    if(cartCtx.items.length===0){
+      return;
+    }
+    setbtnIsHighLighted(true);
+
+    const timer=setTimeout(()=>{
+      setbtnIsHighLighted(false);
+    },300);
+
+    return()=>{
+      clearTimeout(timer);
+    }
+
+  },[items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
